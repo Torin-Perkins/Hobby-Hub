97cf2367e86bfda59ffbeff53d2d1090e0396cdb
Nividh Singh <Nividh.singh@gmail.com>
2020-07-27 07:39:29 -0700

    Adding three category pages

---
+++ Hobby-Hub/Hobby-Hub/Hobby-Hub/Art.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Art
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class Art : ContentPage
+{
+    public Art()
+    {
+        InitializeComponent();
+    }
+}
+}
