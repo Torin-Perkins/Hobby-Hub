9d7bd8c67e3f9ad2fac3d93f986d74fb41eca30c
Nividh S <Rawrocky5280@gmail.com>
2020-07-30 12:05:32 -0700

    Changed App Name

--- XamarinTest/XamarinTest/XamarinTest.Android/MainActivity.cs
+++ XamarinTest/XamarinTest/XamarinTest.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace XamarinTest.Droid
 {
-    [Activity(Label = "XamarinTest", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "HobbyHub", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
--- XamarinTest/XamarinTest/XamarinTest.Android/obj/Debug/90/android/environment.x86_64.s
+++ XamarinTest/XamarinTest/XamarinTest.Android/obj/Debug/90/android/environment.x86_64.s
@@ -72,7 +72,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"3cd171a4-c202-47ac-a233-8da7b5472702"
+	.asciz	"f6fb666f-288d-4864-893b-d7dc019642c6"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
